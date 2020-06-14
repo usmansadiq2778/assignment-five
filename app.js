25671975285a4307c6bf72b01ddd37471514b1c8
usmansadiq <mianusman2488@gmail.com>
2020-06-14 05:43:26 -0700

    Assignment five

---
+++ app.js
@@ -0,0 +1,500 @@
+
+
+
+        // chp 1 (1)
+alert("welcom to  our website");
+
+
+
+       //   chp 1 (2)
+var a="Error! Please enter a valid password";
+alert(a)
+
+    
+
+      //   chp 1 (3)
+var a="Welcom to js Land..."; 
+var b="Happy codding!"
+
+alert(a + '\n' + b);
+
+
+         //  chp 1 (4)
+  
+    var a="Welcom to js Land..."; 
+    alert(a)
+
+    var b="Happy Codding!"
+    var c="prevent this page from creating additional dialogs"
+    alert(b + '\n' +c )
+   
+
+    //   chp 1 (5)
+ 
+    var a="Hello.. i can run js through my web browser console";
+    alert(a);
+    console.log(a);
+      
+
+        // chp 1 ====task(6)
+  
+  alert("Hello! I am an alert box!");
+document.write("   <p> Make use of alerts in your new/existing HTML & CSS project.</p>")
+
+    
+   
+    
+
+
+         
+
+
+
+
+//         // chp 1 (7)(A)
+
+// //         <!DOCTYPE html>
+// // <html lang="en">
+// // <head>
+// //     <meta charset="UTF-8">
+// //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// //     <title>Document</title>
+// /* <script src="app.js"></script> */
+    
+// // </head>
+// // <body>
+
+// // </body>
+// // </html>
+
+//      // chp 1 (7)(b)
+
+// //         <!DOCTYPE html>
+// // <html lang="en">
+// // <head>
+// //     <meta charset="UTF-8">
+// //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// //     <title>Document</title>
+
+    
+// // </head>
+// // <body>
+
+
+// /* <p>Click the button to display an alert box.</p>
+
+// <button onclick="myFunction()" style="color: blue;">Try it</button>
+
+// </p> */
+// /* <script src="app.js"></script> */
+// // </body>
+// // </html>
+
+//     // chp 1 (7)(c)
+
+// //         <!DOCTYPE html>
+// // <html lang="en">
+// // <head>
+// //     <meta charset="UTF-8">
+// //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// //     <title>Document</title>
+
+    
+// // </head>
+// // <body>
+// /* <script src="app.js"></script> */
+
+// /* <p>Click the button to display an alert box.</p>
+
+// <button onclick="myFunction()" style="color: blue;">Try it</button>
+
+// </p> */
+
+// // </body>
+// // </html>
+//     // chp 1 (7)(d)
+
+
+
+
+
+
+
+//          // chp 2        
+
+
+               //  Q #1
+        var username;
+        
+       
+
+                      // Q #2
+        var myname;
+        myname="usman sadiq"
+
+                               // Q #3
+        var a;
+        a="Hello World"
+        alert(a)
+
+
+                               //   Q # 4
+              var name="usman sadiq"
+              alert(name)
+              var age =15+" years old";
+              alert(age);
+              var qulification="Certified Mobile Application Development";
+              alert(qulification);
+
+
+
+
+
+                                       //  Q # 5
+
+                var a="pizaa";
+                
+              
+                alert(a)
+               
+
+                    
+                
+                                          // Q #6 
+                var a ="My Email Address is mainusman"+2488+"@"+"gmail.com";
+                 alert(a);
+
+                                       //  Q #7
+                var Book; 
+                Book= " A smarter \n way to learn  JavaScript ";
+
+                alert(" i am trying to learn from the Book. " + Book);
+                 
+               
+                                         // Q #8
+                   var a;
+                   a="Yeah! I can write HTML content through javaScript";
+
+
+
+                   document.write(a);
+
+
+//                                      //  Q # 9
+                var a;
+                a="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+                alert(a);
+
+                    
+
+                    //  chp # 3(1)
+    
+     var a;
+     a=" i am "+ 15 +" year old  "
+     alert(a)                
+
+                            // chp # 3(2)
+
+                           
+
+
+
+                            // chp # 3(3)
+      var BirthYear;  
+            BirthYear="My birth Year is " + 1998  ;
+            document.write(BirthYear + "<br>" + " Data type of my declared variable is Number");
+
+                     
+                        //  chp # 3(4)
+
+
+var Visitorsname=prompt(" Visitor’s name ");
+var Producttitle=prompt(" Product title ")
+var Quantity=prompt(" Quantity i.e. how many products a visitor wants to order ")
+document.write(Visitorsname,"  " + Producttitle," " + " order "+ Quantity +" "+"on XYZ Clothing store")
+ 
+
+
+     
+                //  chp 4 (1)
+                // DECLARE @Var1 INT, @Var2 INT;
+                //    SET @Var1 = 1;
+                //    SET @Var2 = 2;
+                //    SELECT @Var1 'Var1', @Var2 'Var2'
+                
+
+                // chp 4(2)
+                // leagel variable;
+                var price_9̶9̶_89;
+                var π = Math.PI;
+                var ಠ_ಠ = eval;
+                var Ⅳ = 4;
+                var  〱〱 = 2;
+                  //  ileagel variable
+                //    var a b="usman";
+                //    var P2_a%88$;
+                // var 6ali="bilal";
+                // var ali_(bilal)=66;
+
+                                      //  chp 4 task(3)
+                    (a)
+                    var a="A variable name can contain only letters, numbers, dollar signs, and underscores."
+                    alert("Rules for naming Js variable")
+                    document.write(a)
+                      
+                    // chp 4 task(4)
+                    var $a_b;
+                   
+                    $a_b="_________, _________, _________  and  _________.";
+                    document.write($a_b); 
+
+                    // chp 4 task(5)
+                    var name=" ______________, ";
+                    var $name=" _______________ ";
+                    var _name=" ________________. ";
+                    document.write(name,$name +"or"+_name);
+                    
+                    
+                                        //    ======== chp 4 TextTrackList(6) 
+                    var a="variable name are case "
+                    var b;
+                    alert(a);
+                    document.write("$ ,"+ " " +"a__b ," + " anyNumber 9 ," + " dollar signs $ ," + " underscores")
+
+
+                                                // chp 4 TextTrackList(7) 
+                   var b="variable names shouid not be JS "
+                   alert(b);
+                   document.write("@ ," + " cannot contain any space," + "5ali, ","11")
+                 
+
+
+
+
+                            // chp 5 task 1
+                            var a= 3;
+                            var b = 5;
+                            document.write(" sum of 3 and 5 is " + (a+b));
+
+
+                             // chp 5 task 2
+                             var a= 10;
+                             var b = 5;
+                             document.write(" sum of 5 and 10 is = " + (a+b) + "<br>");
+                             document.write(" subtraction of 5 and 10 is = " + (a-b) + "<br>");
+                             document.write(" division of 10 and 5 is = " + (a/b) + "<br>");
+                             document.write(" multiplication of 10 and 5 is = " + (a*b) + "<br>");
+                             document.write(" modulus of 10 and 5 is = " + (a%b) + "<br>");
+
+
+
+
+                               // chp 5 task 3
+                               var a;
+                               a="value after variable declaration is:";
+                               b=5;
+                               document.write(a +" <br>" )
+                               document.write("Initial value:" +b +" <br>")
+                               document.write("5+1"+"<br>")
+
+                               document.write("Value after increment is:"+6 +"<br>")
+                               document.write("ADD 7 to the previous variable: " + "  6 + 7 = " + ( 6+7 ) + "<br>")
+                               document.write("Value after addition is:" +" 13" + "<br>")
+                               document.write("Decrement the variable" + "<br>");
+                               document.write("Value after decrement the variable : 13 - 1 = " +(13-1)+ "<br>");
+                               document.write("remainder  after the deviding the variable: " + " 12 % 3 = " + ( 12 % 3 )+"<br>")
+                                         document.write("output the remainder is : 0")
+                               
+                             
+                        
+
+                                      //  chp 5 task 4
+
+              var ticat_price=600;
+              document.write("Total cost to buy 5 ticket to a movie is " + (ticat_price * 5 ) + "PKR")
+
+
+                                     //  chp 5 task 5
+
+                  var a;
+                document.write("Table  of 4" +"<br>");
+                for( var i=1;i<=10;i++){
+                   document.write("4 x" + i  +" = " + 4*i +"<br>");
+                   
+                }
+               var f =25;
+               document.write(("25C is 77f" + "<br>" ));
+               
+               document.write("70 f is 21.111111111C") 
+               
+               
+  
+                               // chp 5 task 6
+
+                        var item1=650;
+                        var item2=100;
+                        document.write("<h1>Shoping Cart</h1>");
+
+                        document.write("Price of item 1 is : " + item1 +"<br>")
+                        document.write("Quentity of item 1 is : " +3 +"<br>")
+                        document.write("price of item 2 is : " + item2 +"<br>")
+                        document.write("Quentity of item 2 is : " + 7 +"<br>")
+                        document.write("Shipping charges :100" +"<br>")
+                        document.write("Total cost of your order : " +( (item1*3)+(item2 * 7)+  100));
+                        
+                                
+
+
+
+                        var stdent_marks =804;
+                        var total_marks=980;
+                        document.write("<h1>Marks sheet</h1>")
+                        document.write("Total Marks is : " + total_marks +" <br>");
+                        document.write("Marks Obtain : " + stdent_marks + " <br> ")
+                        document.write("Percentage is : " + (804/980*100));
+                    
+                           
+
+                            // Chp 5 task 9
+
+                        var us_dolar = 10;
+                        var soudi_rial = 25;
+                        document.write("<h1>Currency in PKR</h1>" +"<br>");
+                        document.write("Total Currency in PKR : " + (us_dolar * 104.80 + soudi_rial * 28))
+               
+
+
+                          // Chp 5 task 10
+
+                    
+             var a =10;
+             document.write("initialize a variable = 10" + "<br>")
+             document.write("Add 5 " + "  = " + (a + 5 + " <br>"))   
+             document.write("Divide the result by 2 " + "  = " + ((a+5)/2  + " <br>"))
+             document.write(" Perfome all Calculation in single expresstion " + ((a+5)+((a+5)/2)) );           
+              
+
+                       
+                   // chp 5 task 11
+
+          var current_year = 2020
+          var age = 21;
+          var birth_year = 1998  
+          document.write("<h1> Age Calculator </h1>" + "<br>")
+          document.write("Current year is : " + current_year + "<br>")
+          document.write("Birth Year is : " + birth_year + "<br>")
+          document.write("Your Age is : " + age + "<br>")
+
+
+
+                                  // chp 5 task 12
+
+            
+         var radias=20;
+         document.write("<h1>The Geomatrizer</h1>" +"<br>")
+         var cercumfernce ;
+         cercumfernce = 2 * 3.142 * 20;
+         var area;
+         area = 3.142 * (20 * 20);
+         document.write("Circumference is : " + cercumfernce + " <br>");
+         document.write("The Area is : " + area)  
+         
+         
+         
+         
+                                // chp 5 task 13
+                              var favorite = "Chilimili";
+                              var age = 20;
+                              var maxage=65;
+                              var amont = 5;
+                              document.write("<h1>The Lifetime Supply Calculator</h1>");
+                              document.write("Favourite Snake : " + favorite +"<br>")
+                              document.write("Current age : " + age +"<br>");
+                              document.write("Estimated maximum Age : " + maxage +"<br>");
+                              document.write("Amount of Snaks per day : " + amont +"<br>");
+                              document.write("You Will need " +225+" chilimili to last you untill the ripe old age of 65")
+
+                              
+
+
+
+
+
+
+
+
+
+                              // chp 6   task 1
+                              
+                              var a = 20;
+                              document.write("<h1> Results</h1>")
+                              document.write("The value of a is : " + a +"<br>");
+
+                              document.write("........................................"+"<br>")
+                              document.write("  "+"<br>")
+                              document.write("The value of ++a is : " + (++a)+"<br>");
+                              document.write("Now  value of a is : " + (a)+"<br>");
+                              document.write("  "+"<br>")
+                              document.write("The value of a++ is : " + (a++)+"<br>");
+                              document.write(" Now  value of a is : " + (a)+"<br>");
+                              document.write("  " + "<br>")
+                              document.write("The value of --a is : " + (--a)+"<br>");
+                              document.write("Now  value of a is : " + (a)+"<br>");
+                              document.write("  "+"<br>")
+                              document.write("The value of a-- is : " + (a--)+"<br>");
+                              document.write(" Now  value of a is : " + (a)+"<br>");
+
+
+                             
+                              // chp 6 task 2
+                              var a = 2;
+                             var b = 1;
+                             document.write("<h1>Result</h1>") 
+                             var results;
+                             results= (--a)-(--b) + (++b)+(b--);
+                             document.write( " All results : "+ results + "<br>") 
+                             document.write("--a : "+ (--a) + "<br>") 
+                             document.write("  --a -  --b : "+ ((--a)-(--b)) + "<br>")
+                             document.write(" --a - --b  + ++b : "+ ((--a)-(--b) + (++b)) + "<br>")
+                             document.write(" --a - --b  + ++b + b-- : "+ ((--a)-(--b) + (++b)+(b--)) + "<br>")                        
+
+
+
+
+                                        //  chp 6 task 3
+
+                                       var a=prompt("enter the user name ")
+                                       document.write(a)
+
+
+                                        //  chp 6 task 4
+                                       var a=prompt("enter a number you want to print table")
+                                       document.write("Table is : " +a +"<br>")
+                                      for(var i=1;i<=10;i++){
+                                    document.write( a + " x " + i + " = " + i * a  +" <br>")
+                                     if(a===""){
+                                      document.write( " 5 x " + i + " = " + i * 5  +" <br>")
+                                        }
+                                   }
+
+
+
+
+                    //  chp 6 task 6
+                     var e =  prompt("Enter eng marks ");
+                     var u =  prompt("Enter urdu marks");
+                     var m =  prompt("Enter math marks ");
+                   
+                     var k =  prompt("Enter name ");
+                     var j =  prompt("Enter name");
+                     var o =  prompt("Enter name marks ");
+                   
+                     var t_M=100;
+                     for(var i=0;i<e.length;i++){
+                      for (var j=0;j<i;j++){
+                        document.write(e +"<br>"+u +"<br>")
+                      }
+                     }
+                     
+
+                  
